bd5a703a8d1bbde3d04fb5a83ee388e0183eefd7
Yuya Kasai <moyoyomiyazawa@gmail.com>
2021-05-22 17:54:48 +0900

    Navコンポーネントまで実装

---
+++ pages/index.jsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Header } from '../components/Header'
+import { Nav } from '../components/Nav'
+import { Results } from '../components/Results'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hulu 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <Nav />
+      <Results />
+    </div>
+  )
+}
+
+export async function getSeverSideProps(context) {
+
+}
